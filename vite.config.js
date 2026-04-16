a49909d943944a780707f8205176581cb5b39a60
Lwatherbazar.com.pk <leatherbazar.com.pk@gmail.com>
2026-04-16 19:59:54 +0500

    Add preview configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, open: true }
+  server: { 
+    port: 3000, 
+    open: true 
+  },
+  preview: {
+    allowedHosts: ['tat-bbc9.onrender.com']
+  }
 })
